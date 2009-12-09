eb5bae4fe8f2b7f29666297449a1a4b7da8dafee
Mike Williams <mdub@dogbiscuit.org>
2009-12-09 23:39:19 +1100

    Make books unique on title.

--- app/models/shiva_loader.rb
+++ app/models/shiva_loader.rb
@@ -4,9 +4,19 @@ class ShivaLoader
     @book ||= Book.create!
   end
 
+  def title=(title)
+    if @book
+      unless @book.title == title
+        raise "Can't load content for #{title.inspect} into #{@book.title.inspect}"
+      end
+    else
+      @book = Book.find_or_create_by_title(title)
+    end
+  end
+  
   def load(xml)
     doc = Nokogiri::XML.parse(xml)
-    book.title ||= doc.xpath("/book-content/book/title").first.text
+    self.title = doc.xpath("/book-content/book/title").first.text
     doc.xpath("/book-content/content").each do |content_element|
       section_path = content_element.xpath("sections//title").map(&:text)
       section = find_or_create_section(section_path)
--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "mongo_mapper", "0.6.4"
   config.gem "nokogiri", "1.4.0"
+  config.gem "rack", "1.0.1"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
