6a745a71da128bdbce7f0588fd2b51c0729ac234
Mike Williams <mdub@dogbiscuit.org>
2009-12-10 09:48:18 +1100

    Add breadcrumb trail.

--- app/helpers/section_helper.rb
+++ app/helpers/section_helper.rb
@@ -6,4 +6,19 @@ module SectionHelper
     link_to(link.label, subsection_path)
   end
   
+  def book_section_breadcrumb_links
+    section_labels = params[:labels].map(&CGI.method(:unescape))
+    links = []
+    1.upto(section_labels.length) do |n|
+      labels = section_labels[0, n]
+      escaped_labels = labels.map(&CGI.method(:escape))
+      links << link_to_unless_current(labels.last, book_section_path(@book, escaped_labels))
+    end
+    links
+  end
+  
+  def book_section_breadcrumb_trail
+    book_section_breadcrumb_links.join(" / ")
+  end
+  
 end
