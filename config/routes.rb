4a796c9b63166cce2aecccbd83a585c2a3a0bfc2
Mike Williams <mdub@dogbiscuit.org>
2009-12-18 08:24:10 +1100

    More messing with routes; use path_prefix.

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ ActionController::Routing::Routes.draw do |map|
   map.books 'books', :controller => "books"
   map.book 'books/:title', :controller => "books", :action => "show"
 
-  map.with_options(:controller => "book_sections") do |section|
-    section.edit_book_section   'books/:title/*section_keys/edit', :conditions => { :method => :get }, :action => "edit"
-    section.update_book_section 'books/:title/*section_keys/edit', :conditions => { :method => :put }, :action => "update"
-    section.book_section        'books/:title/*section_keys/show', :action => "show"
+  map.with_options(:controller => "book_sections", :path_prefix => "books/:title/*section_keys") do |section|
+    section.edit_book_section   '~edit', :conditions => { :method => :get }, :action => "edit"
+    section.update_book_section '~edit', :conditions => { :method => :put }, :action => "update"
+    section.book_section        '', :action => "show"
   end
 
 end
