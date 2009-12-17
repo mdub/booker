6b2e10d892e803c2987ed48e72133ec8a4e2d913
Mike Williams <mdub@dogbiscuit.org>
2009-12-18 08:21:25 +1100

    Mess with routes.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ ActionController::Routing::Routes.draw do |map|
   map.with_options(:controller => "book_sections") do |section|
     section.edit_book_section   'books/:title/*section_keys/edit', :conditions => { :method => :get }, :action => "edit"
     section.update_book_section 'books/:title/*section_keys/edit', :conditions => { :method => :put }, :action => "update"
-    section.book_section        'books/:title/*section_keys', :action => "show"
+    section.book_section        'books/:title/*section_keys/show', :action => "show"
   end
 
 end
